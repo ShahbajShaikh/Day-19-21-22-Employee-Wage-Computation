fdb11f95fa77e3cfc2a5f618c2ea99c4274310aa
shahbaj <shahbaj.49408@gmail.com>
2020-10-29 12:59:11 +0530

    Added: UC7 Refactoring the Code.

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -169,3 +169,43 @@ public class EmpWageComputation {
 			System.out.println("Total Emp Wage: " +totalEmpWage);
    }
 }
+
+
+//Refactoring the code UC-7
+
+public class EmpWageComputation {
+        public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMP_RATE_PER_HOUR = 20;
+        public static final int NUM_OF_WORKING_DAYS = 20;
+        public static final int MAX_HRS_IN_MONTH = 100;
+
+        public static void EmpWagesMonthly(){
+                int empHours=0;
+                int totalWorkingDays=0, totalEmpHours=0;
+                int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+                while (totalEmpHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+                        totalWorkingDays++;
+                        switch(empCheck){
+                                case IS_PART_TIME:
+                                        empHours=8;
+                                        break;
+                                case IS_FULL_TIME:
+                                        empHours=4;
+                                        break;
+                                default:
+                                        empHours=0;
+                        }
+                totalEmpHours += empHours;
+                }
+        System.out.println("Total Employee Working Hour - "+totalEmpHours);
+        System.out.println("Total Employee Working Days - "+totalWorkingDays);
+        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+        System.out.println("Total employee wage - "+totalEmpWage);
+        }
+
+        public static void main(String[] args){
+                System.out.println("Welcome to Employee Wage Computation");
+                EmpWagesMonthly();
+        }
+}
