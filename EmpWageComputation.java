0128c25e6e984aa47994e2e30d6c471f41600695
shahbaj <shahbaj.49408@gmail.com>
2020-11-03 13:55:31 +0530

    Added: UC9- Save Total Wage of Company.

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -253,3 +253,65 @@ public class EmpWageComputation {
 		empMonthlyWages(wagePerHour,numWorkingDays,numWorkingHours);
 	}
 }
+
+//UC9-Save Total Wage of company
+
+import java.util.Scanner;
+public class EmployeeWageComputation {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	private String company;
+	private int wagePerHour;
+	private int workingDays;
+	private int workingHours;
+	private int totalEmpWage;
+
+	public EmployeeWageComputation(String company ,int wagePerHour,int workingDays,int workingHours){
+		this.company=company;
+		this.wagePerHour=wagePerHour;
+		this.workingDays=workingDays;
+		this.workingHours=workingHours;
+	}
+
+	//Calculating monthly wage of employee
+	public void employeeMonthlyWage(){
+		//variables
+		int empHours=0;
+		int totalWorkingDays=0, totalEmpHours=0;
+		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+		while (totalEmpHours <= workingHours && totalWorkingDays < workingDays){
+                        totalWorkingDays++;
+			switch(empCheck){
+				case IS_PART_TIME:
+					empHours=8;
+					break;
+				case IS_FULL_TIME:
+					empHours=4;
+					break;
+				default:
+					empHours=0;
+			}
+		totalEmpHours += empHours;
+		}
+	System.out.println("total employee working hour - "+totalEmpHours);
+        System.out.println("total employee working days - "+totalWorkingDays);
+        totalEmpWage = totalEmpHours * wagePerHour;
+	}
+
+	public String toString(){
+        	return "Total employee wage for company : "+company+" is - "+totalEmpWage;
+	}
+
+	public static void main(String[] args){
+		System.out.println("Welcome to Employee Wage Computation");
+		//take user input for each company
+		EmployeeWageComputation tata = new EmployeeWageComputation("Tata",20,20,100);
+		EmployeeWageComputation reliance = new EmployeeWageComputation("Jio",10,25,150);
+		tata.employeeMonthlyWage();
+		System.out.println(tata);
+		jio.employeeMonthlyWage();
+		System.out.println(jio);
+	}
+}
+
